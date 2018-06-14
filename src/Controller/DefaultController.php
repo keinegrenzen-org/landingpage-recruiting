b758233bc623187d1033dd5256d9ac31f0659ba2
Barthy <b.bonhomme@vucx.de>
2018-06-14 14:29:48 +0200

    design progress

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
+
     /**
-     * @Route("/", name="index")
+     * @Route("/{name}", name="index", defaults={"name" = "Musik"})
+     * @param string $name
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(string $name)
     {
         return $this->render('index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'name' => $name,
         ]);
     }
 }
